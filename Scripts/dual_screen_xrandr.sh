c636e0424919d8d5fdee8ddb7037a7ac2f89bf9c
Atif <atif.anwer@insa-rouen.fr>
2021-09-21 10:07:27 +0200

    dropbox startup and some cleanup

--- Scripts/dual_screen_xrandr.sh
+++ Scripts/dual_screen_xrandr.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xrandr --auto --output DP-4 --mode 2560x1600 --right-of HDMI-0
 xrandr --output HDMI-0 --rotate left
-xrandr --output DP-4 --brightness 0.5
+xrandr --output DP-4 --brightness 0.75
