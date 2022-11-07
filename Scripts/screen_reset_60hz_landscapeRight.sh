edc130e7f39722ee829aa6001d82c091d118f31d
Atif <atif.anwer@insa-rouen.fr>
2022-11-07 10:16:46 +0100

    fixed DPI multi-monitor scaling issue

--- Scripts/screen_reset_60hz_landscapeRight.sh
+++ Scripts/screen_reset_60hz_landscapeRight.sh
@@ -4,6 +4,6 @@ echo "Resetting screen ..."
 # xrandr --output DP-0 --off --output DP-1 --off --output DP-3 --off --output HDMI-0 --off
 xrandr -s 0
 xrandr --auto
-xrandr --output HDMI-0 --mode 1920x1080 --pos 2560x0 --rotate normal --rate 60 --right-of DP-4
-xrandr --output DP-4 --primary --mode 2560x1600 --pos 0x0 --rotate normal --brightness 0.75 --rate 60
+xrandr --output HDMI-0 --mode 1920x1080 --pos 2560x0 --rotate normal --rate 60 --right-of DP-4 --dpi 220
+xrandr --output DP-4 --primary --mode 2560x1600 --pos 0x0 --rotate normal --brightness 0.75 --rate 60 --dpi 220
 nitrogen --restore
