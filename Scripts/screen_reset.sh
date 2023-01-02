de6d35fed6da9318b9cd7ec33bb58b23b19329cc
Atif <atif.anwer@insa-rouen.fr>
2023-01-02 10:20:49 +0100

    pre-cleanup polybar

--- Scripts/screen_reset.sh
+++ Scripts/screen_reset.sh
@@ -3,5 +3,5 @@
 xrandr -s 0
 xrandr --auto
 echo "Resetting screen ..."
-xrandr --output DP-4 --primary --rate 144 --output DP-1-3 --left-of DP-4
+xrandr --output DP-4 --primary --rate 165 --output DP-1-3 --left-of DP-4
 nitrogen --restore
