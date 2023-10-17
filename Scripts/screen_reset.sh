c362149d1672358bb16a725f6d0bb763df4f9b3d
Atif-Anwer <atif.anwer@insa-rouen.fr>
2023-10-17 11:11:10 +0200

    Updated neofetch and dual display for ultragear

--- Scripts/screen_reset.sh
+++ Scripts/screen_reset.sh
@@ -3,5 +3,5 @@
 xrandr -s 0
 xrandr --auto
 echo "Resetting screen ..."
-xrandr --output DP-4 --primary --rate 165 --output DP-1-3 --left-of DP-4
-nitrogen --restore
+xrandr --output DP-4 --rate 165 --output DP-2 --primary --right-of DP-4 --rate 165
+nitrogen --restore --force-setter=xinerama
