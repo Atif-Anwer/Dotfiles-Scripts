77f209ab5f9449dc7db65f0881ca5571b5c455ec
atif-anwer <atif_anwer@hotmail.com>
2025-12-22 23:14:57 +0500

    MacOS with Sketchybar and Aerospace

---
+++ sketchybar/helpers/default_font.lua
@@ -0,0 +1,14 @@
+return {
+	text = "JetBrainsMono Nerd Font", -- Used for text
+	numbers = "JetBrainsMono Nerd Font", -- Used for numbers
+	weather = "Symbols Nerd Font",
+
+	-- Unified font style map
+	style_map = {
+		["Regular"] = "Regular",
+		["Semibold"] = "Semibold",
+		["Bold"] = "Bold",
+		["Heavy"] = "Heavy",
+		["Black"] = "Black",
+	}
+}
